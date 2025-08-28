4446f1cfcead452d485312153ebc6494e9823754
Sujal1323 <Sujal1323.sujalbc49@gmail.com>
2025-08-28 23:20:55 +0545

    assignment completed

--- src/q9.c
+++ src/q9.c
@@ -1,19 +1,18 @@
-// Input a number representing days and print the equivalent number of weeks and days.
-// Example: 
-// Input = 10
-// Output = "1 week and 3 days"
 #include <stdio.h>
-
 int main() {
     int totalDays, weeks, days;
+
     printf("Enter number of days: ");
     scanf("%d", &totalDays);
-    weeks = totalDays / 7;
-    days = totalDays % 
+
+    weeks = totalDays / 7; 
+    days = totalDays % 7;  
+
     printf("%d week%s and %d day%s\n", 
            weeks, (weeks == 1 ? "" : "s"), 
            days, (days == 1 ? "" : "s"));
 
     return 0;
 }
+
 
