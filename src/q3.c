2f16c36ca1e637fbc67ef6204caebec6521d195e
Sujal1323 <Sujal1323.sujalbc49@gmail.com>
2025-08-28 23:02:28 +0545

    assignment completed

--- src/q3.c
+++ src/q3.c
@@ -6,6 +6,6 @@ int main() {
     char name[50];
     printf("Enter your name");
     scanf("%s",name);
-    printf("Hello,%s! Welcome to the program.\n");
+    printf("Hello,%s! Welcome to the program.\n", name);
     return 0;
 }
